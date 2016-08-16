f7bc6aeac5ff27b43900370b7567d536b4056fe9
lhzhou <3692281@qq.com>
2016-08-16 10:38:41 +0800

    update

--- config/app.php
+++ config/app.php
@@ -147,6 +147,9 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
+
 
         /*
          * Application Service Providers...
